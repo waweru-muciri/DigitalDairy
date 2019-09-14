b9a62e96d32a1829232fd472c1688811fa488ce3
Brian Muciri <bwwaweru18@gmail.com>
2019-09-14 07:36:04 +0300

    Additional changes

--- digitaldairy/templates/digitaldairy/service-worker.js
+++ digitaldairy/templates/digitaldairy/service-worker.js
@@ -1,5 +1,3 @@
-import { ClientHttp2Session } from "http2";
-
 // // Give the service worker access to Firebase Messaging.
 // // Note that you can only use Firebase Messaging here, other Firebase libraries
 // // are not available in the service worker.
